8b06d0b3908e38f9b52cac36864fb75e06d3149f
Trent Gebben <TGebben@jsitel.com>
2018-11-01 15:03:30 -0500

    update requirements

--- Interactive Visualizations and Dashboards HW/StarterCode/Belly_Button_Biodiversity/static/js/app.js
+++ Interactive Visualizations and Dashboards HW/StarterCode/Belly_Button_Biodiversity/static/js/app.js
@@ -4,26 +4,86 @@ function buildMetadata(sample) {
 
   // Use `d3.json` to fetch the metadata for a sample
     // Use d3 to select the panel with id of `#sample-metadata`
+  
+  d3.json("/metadata/" + sample).then(successhandel).catch(errorhandel)
+  function successhandel(results){
+    console.log(results );
+    var x=Object.entries(results)
+    console.log( `x:${x}`);
+    var sample_data= d3.select("#sample-metadata");
 
     // Use `.html("") to clear any existing metadata
+
+    sample_data.html("");
 
     // Use `Object.entries` to add each key and value pair to the panel
     // Hint: Inside the loop, you will need to use d3 to append new
     // tags for each key-value in the metadata.
 
-    // BONUS: Build the Gauge Chart
-    // buildGauge(data.WFREQ);
+    Object.entries(results).forEach(function(currentValue , index){
+      console.log(currentValue)
+      sample_data.append("p").text(`${currentValue[0]}: ${currentValue[1]}`).style("font-size", "12px").style("font-weight", "bold")
+    
+    })
+  };
+
+  function errorhandel(error){
+    console,log(error);
+  };
+
 }
 
 function buildCharts(sample) {
 
   // @TODO: Use `d3.json` to fetch the sample data for the plots
+  d3.json("/samples/" + sample).then(successfulfun).catch(errorfun)
+  function successfulfun(data){
+    // print data 
+    console.log(data.otu_ids)
+    console.log(data.sample_values)
+    console.log(data.otu_labels)
 
     // @TODO: Build a Bubble Chart using the sample data
+    var traceBubble = {
+      x : data.otu_ids,
+      y : data.sample_values,
+      mode: 'markers',
+      marker : { 
+        color : data.otu_ids,
+        size : data.sample_values,
+        colorscale :'Earth'
+      },
+      hoverinfo :`${data.otu_ids} : ${data.otu_labels} `
+    };
+    
+    var dataBubble = [traceBubble];
+    var layout = {
+      xaxis : { title : 'OTU ID' }
+    };
+
+    Plotly.newPlot("bubble", dataBubble, layout);
 
     // @TODO: Build a Pie Chart
     // HINT: You will need to use slice() to grab the top 10 sample_values,
     // otu_ids, and labels (10 each).
+    var tracePie = {
+      type : "pie",
+      values : data.sample_values.slice(0,10),
+      labels : data.otu_ids.slice(0, 10),
+      textinfo : "percent",
+      text : data.otu_labels.slice(0, 10),
+      hoverinfo : "label,text,value,percent"
+    }
+
+    var dataPie = [tracePie]
+    Plotly.newPlot("pie", dataPie)
+    
+    }
+  
+  function errorfun(error){
+    console.log(error)
+  }
+
 }
 
 function init() {
